7c660dde9d9c7de5969907ff3ff24380949a2092
Serhii Kapitan <serejkakaapitan@gmail.com>
2026-04-03 21:37:26 +0300

    add newsSlider

--- frontend/src/components/sections/NewsSlider/NewsSlider.jsx
+++ frontend/src/components/sections/NewsSlider/NewsSlider.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-export const NewsSlider = () => {
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+
+import { Card } from '../../shared/Card/Card';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import styles from './NewsSlider.module.css';
+
+export const NewsSlider = ({ items }) => {
   return (
-    <div>NewsSlider</div>
-  )
-}
+    <div className={styles.sliderWrap}>
+      <Swiper
+        modules={[Navigation]}
+        navigation
+        spaceBetween={20}
+        slidesPerView={'auto'}
+        className={styles.slider}
+      >
+        {items.map(item => (
+          <SwiperSlide key={item.id} className={styles.slide}>
+            <Card image={item.image} title={item.title} text={item.text} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Header } from '../components/shared/Header/Header';
 import { SectionCard } from '../components/shared/SectionCard/SectionCard';
 import {
   NewsSlider,
@@ -7,13 +6,49 @@ import {
   DonateBlock,
 } from '../components/sections/index';
 
+import img1 from '../assets/news1.jpeg';
+import img2 from '../assets/news2.jpeg';
+import img3 from '../assets/news3.jpeg';
+import img4 from '../assets/news4.jpg';
+
+const newsItems = [
+  {
+    id: 1,
+    image: img1,
+    title: 'Тестова новина №1',
+    text: 'Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_но',
+  },
+  {
+    id: 2,
+    image: img2,
+    title: 'Тестова новина №2',
+    text: 'Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_но',
+  },
+  {
+    id: 3,
+    image: img3,
+    title: 'Тестова новина №3',
+    text: 'Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_но',
+  },
+  {
+    id: 4,
+    image: img4,
+    title: 'Тестова новина №4',
+    text: 'Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_новини_Текст_но',
+  },
+];
+
 export const HomePage = () => {
   return (
     <>
       <SectionCard title="Новини притулку" btnText="Всі новини >" to="/news">
-        <NewsSlider />
+        <NewsSlider items={newsItems} />
       </SectionCard>
-      <SectionCard title="Підтримати приют" btnText="Інша допомога >" to='/news'>
+      <SectionCard
+        title="Підтримати приют"
+        btnText="Інша допомога >"
+        to="/catalog"
+      >
         <DonateBlock />
       </SectionCard>
       <SectionCard title="Знайти друга" btnText="Усі тваринки >" to="/catalog">
