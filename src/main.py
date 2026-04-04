773b6c8616a731b067fdd9c997dd3e1ac24364f8
SamanuelAdmin <samoolenkosvatik93@gmail.com>
2026-04-04 05:22:54 +0000

    add cors

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .accounts import AccountsRouter, load_models
 from .database import DatabaseConnection
