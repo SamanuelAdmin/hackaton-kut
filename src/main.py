ee19065f6077f9f1e20d0114df117643dd50d5a6
SamanuelAdmin <samoolenkosvatik93@gmail.com>
2026-04-04 04:06:13 +0000

    remove prefix from main

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from pets import PetsRouter
 app = FastAPI()
 
 # including routers
-app.include_router(PetsRouter, prefix="/api/v1")
+app.include_router(PetsRouter)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
