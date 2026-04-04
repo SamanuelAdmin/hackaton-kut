0340d118085bddc536564b5abe674cafe90b3b05
SamanuelAdmin <samoolenkosvatik93@gmail.com>
2026-04-04 05:12:11 +0000

    add cors

--- src/main.py
+++ src/main.py
@@ -9,5 +9,17 @@ app = FastAPI()
 # including routers
 app.include_router(PetsRouter)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:1337",
+        "http://127.0.0.1:8000",
+        "https://pets.xhosts.xyz",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
